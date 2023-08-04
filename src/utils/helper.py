94ec2075b6fc9600edd93a82bbbaa1ddbb966c4e
xdssio <jonathan@xdss.io>
2023-08-04 11:54:33 +0200

    add lakefs and s3

--- src/utils/helper.py
+++ src/utils/helper.py
@@ -12,39 +12,70 @@ class Helper:
     DVC = "dvc"
     LFS_S3 = "lfs-s3"
     LFS_GITHUB = "lfs-github"
-    XETHUB = "xethub"
+    LFS = "lfs"
+    XETHUB_PY = "xethub_pyxet"
+    XETHUB_GIT = "xethub_git"
+    XETHUB = 'xethub'
+    LAKEFS = "lakefs"
+    S3 = "s3"
+    M1 = "m1"
 
     def __init__(self):
+
+        self.fs = pyxet.XetFS()
+        self.xet_pyxet_repo = self._get_xet_repo(Helper.XETHUB_PY)
+        self.xet_local_repo = self._get_xet_repo(Helper.XETHUB_GIT)
+
+    @staticmethod
+    def _get_xet_repo(path: str):
         origins = subprocess.run(
-            "git remote -v", shell=True, cwd="xethub", stdout=subprocess.PIPE
+            "git remote -v", shell=True, cwd=path, stdout=subprocess.PIPE
         ).stdout.decode()
         origin = origins.split("\n")[0].split("\t")[1].split(" ")[0].split("/")
-        self.fs = pyxet.XetFS()
-        self.xet_repo = f"xet://{origin[-2]}/{origin[-1].replace('.git', '')}/main"
+        return f"xet://{origin[-2]}/{origin[-1].replace('.git', '')}/main"
 
-    def dvc_upload_new(self, filepath: str):
+    def dvc_new_upload(self, filepath: str):
         return self._dvc_upload(filepath)
 
-    def dvc_upload_merged(self, filepath: str):
+    def dvc_merged_upload(self, filepath: str):
         return self._dvc_upload(filepath)
 
-    def lfs_s3_upload_new(self, filepath: str):
+    def lfs_s3_new_upload(self, filepath: str):
         return self._lfs_upload(filepath, Helper.LFS_S3)
 
-    def lfs_s3_upload_merged(self, filepath: str):
+    def lfs_s3_merged_upload(self, filepath: str):
         return self._lfs_upload(filepath, Helper.LFS_S3)
 
-    def lfs_github_upload_new(self, filepath: str):
+    def lfs_git_new_upload(self, filepath: str):
         return self._lfs_upload(filepath, Helper.LFS_GITHUB)
 
-    def lfs_github_upload_merged(self, filepath: str):
+    def lfs_git_merged_upload(self, filepath: str):
         return self._lfs_upload(filepath, Helper.LFS_GITHUB)
 
-    def xethub_upload_new(self, filepath: str):
+    def xethub_py_new_upload(self, filepath: str):
         return self._xethub_upload(filepath)
 
-    def xethub_upload_merged(self, filepath: str):
+    def xethub_py_merged_upload(self, filepath: str):
         return self._xethub_upload(filepath)
+
+    def xethub_git_new_upload(self, filepath: str):
+        return self._xethub_upload(filepath, pyxet_api=False)
+
+    def xethub_git_merged_upload(self, filepath: str):
+        return self._xethub_upload(filepath, pyxet_api=False)
+
+
+    def lakefs_new_upload(self, filepath: str):
+        return self._lakefs_upload(filepath)
+
+    def lakefs_merged_upload(self, filepath: str):
+        return self._lakefs_upload(filepath)
+
+    def s3_new_upload(self, filepath: str):
+        return self._s3_upload(filepath)
+
+    def s3_merged_upload(self, filepath: str):
+        return self._s3_upload(filepath)
 
     @staticmethod
     def copy_file(filepath: str, repo: str):
@@ -108,13 +139,10 @@ class Helper:
         filename = os.path.basename(filepath)
         if pyxet_api:
             with self.fs.transaction:
-                self.fs.put(filepath, f"{self.xet_repo}/{filename}")
+                self.fs.put(filepath, f"{self.xet_pyxet_repo}/{filename}")
         else:
-            if filename not in set(pathlib.Path(Helper.XETHUB).glob("*")):
-                self.copy_file(filepath, f"{Helper.XETHUB}/{filename}")
-            self._git_upload(filepath, Helper.XETHUB)
+            self._git_upload(filepath, Helper.XETHUB_GIT)
         return pyxet_api
-
 
     def output_upload(self, commit: str):
         self._git_add_commit('output/*', commit=commit)
@@ -132,8 +160,14 @@ class Helper:
         return merged_filepath
 
     def xet_ls(self):
-        return self.fs.ls(self.xet_repo, detail=False)
+        return self.fs.ls(self.xet_pyxet_repo, detail=False)
 
     def xet_remove(self, filename: str):
         with self.fs.transaction:
-            self.fs.rm(f"{self.xet_repo}/{filename}")
+            self.fs.rm(f"{self.xet_pyxet_repo}/{filename}")
+
+    def _lakefs_upload(self, filepath):
+        self.run(f"lakectl fs upload -s {filepath} lakefs://versioning-article/main/{filepath}")
+
+    def _s3_upload(self, filepath):
+        self.run(f"aws s3 cp {filepath} s3://versioning-article/s3/{filepath}")
